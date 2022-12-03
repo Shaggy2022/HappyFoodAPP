94b6f20805e0248da0977305721a0ebc658cd677
Shaggy2022 <jmsanabria93@misena.edu.co>
2022-12-03 03:41:42 +0000

    chore: update entity invoice product

--- src/main/java/com/happyfood/domain/InvoiceProduct.java
+++ src/main/java/com/happyfood/domain/InvoiceProduct.java
@@ -16,8 +16,7 @@ public class InvoiceProduct implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sequenceGenerator")
-    @SequenceGenerator(name = "sequenceGenerator")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
 
